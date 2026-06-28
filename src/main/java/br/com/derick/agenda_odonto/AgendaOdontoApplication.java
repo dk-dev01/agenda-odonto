df598a90e828c38a7105855f5056154ab57c9f84
dk-dev01 <derick.hatakeyama@gmail.com>
2026-06-28 02:42:21 -0300

    chore: criado estrutura inicial do projeto

---
+++ src/main/java/br/com/derick/agenda_odonto/AgendaOdontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.derick.agenda_odonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaOdontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaOdontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/derick/agenda_odonto/AgendaOdontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.derick.agenda_odonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaOdontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
